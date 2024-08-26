3a27f0fb4733cd14e2e9d574153e4ddff436ee7b
Isabella <isabella@sayprimer.com>
2024-08-26 13:37:32 -0300

    Basic Theme with Tailwind and DaisyUI

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -6,9 +6,24 @@ function App() {
   const { data } = useGetQuery();
 
   return (
-    <div>
+    <div className='flex gap-[10%]'>
       <SignIn />
-      {data && <h1>{data}</h1>}
+      <div>
+        {data && <h1>{data}</h1>}
+        <div className='flex flex-wrap gap-2'>
+          <button className="btn">Default</button>
+          <button className="btn btn-neutral">Neutral</button>
+          <button className="btn btn-primary">Primary</button>
+          <button className="btn btn-secondary">Secondary</button>
+          <button className="btn btn-accent">Accent</button>
+          <button className="btn btn-ghost">Ghost</button>
+          <button className="btn btn-link">Link</button>
+          <button className="btn btn-outline">Default</button>
+          <button className="btn btn-outline btn-primary">Primary</button>
+          <button className="btn btn-outline btn-secondary">Secondary</button>
+          <button className="btn btn-outline btn-accent">Accent</button>
+        </div>
+      </div>
     </div>
   )
 }
